a749200c4bcd2a3cf51ec8862873e61ff89fd85d
Raehyeok, Cho <raehyeok.cho@gmail.com>
2025-01-06 18:52:01 +0900

    init commit

---
+++ src/main/java/me/jjorae/health_activity_collector/HealthActivityCollectorApplication.java
@@ -0,0 +1,13 @@
+package me.jjorae.health_activity_collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthActivityCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthActivityCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jjorae/health_activity_collector/HealthActivityCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jjorae.health_activity_collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthActivityCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
